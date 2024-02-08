d7cb74e3acc4419e11310aaa6d019a46173c6342
adhamnaeim <adhamnaeimmansour@gmail.com>
2024-02-08 12:44:55 +0200

    created new field for updated_at date, created migration file, displayed updated at date on index.html

--- tasks/models.py
+++ tasks/models.py
@@ -1,10 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
 class Task(models.Model):
     title = models.CharField(max_length=200)
     status = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True)
     
     def __str__(self):
         return self.title
     
+    def save(self, *args, **kwargs):
+        # Update the 'updated_at' field before saving the object
+        self.updated_at = timezone.now()
+        super(Task, self).save(*args, **kwargs)
